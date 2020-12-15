0bc4378928aaec4afb211423cba3dda220b59c65
Primadika Dwijantara <primadika.dwijantara@gmail.com>
2020-12-15 13:34:06 +0700

    Updates

---
+++ precache-manifest.487c35ac6cb9c1801016ac2f0ee2c6fa.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "c37b511491d0ae134b0a",
+    "url": "/find-and-list-any-anime/static/js/main.168d9c01.chunk.js"
+  },
+  {
+    "revision": "95ea094a8b9a4fe98e05",
+    "url": "/find-and-list-any-anime/static/js/runtime~main.de42bcd0.js"
+  },
+  {
+    "revision": "d4635c5b1de89f4807ca",
+    "url": "/find-and-list-any-anime/static/js/2.61be712d.chunk.js"
+  },
+  {
+    "revision": "cad04380716f72b17ca0867a2157d15b",
+    "url": "/find-and-list-any-anime/index.html"
+  }
+];
